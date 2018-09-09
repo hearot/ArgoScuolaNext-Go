c516eedcf271b24d804679b00bce128b21ae73f3
hearot <gabriel@hearot.it>
2018-09-09 23:58:00 +0200

    Add request

--- argoscuolanext/session.go
+++ argoscuolanext/session.go
@@ -95,3 +95,38 @@ func (c *Credentials) Login() (Session, error) {
 
 	return session, nil
 }
+
+func (s *Session) request(method string, date time.Time) (interface{}, error) {
+	req := requests.Requests()
+
+	var res interface{}
+
+	r, err := req.Get(
+		restApiUrl+method,
+		requests.Header{
+			"Content-Type": "application/json",
+			"x-key-app":    argoKey,
+			"x-version":    argoVersion,
+			"user-agent":   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+			"x-cod-min":    s.credentials.schoolCode,
+			"x-prg-alunno": s.keys["prgAlunno"].(string),
+			"x-prg-scheda": s.keys["prgScheda"].(string),
+			"x-prg-scuola": s.keys["prgScuola"].(string),
+		},
+		requests.Params{
+			"_dc": time.Now().Format("20060102150405"),
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.Json(&res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
